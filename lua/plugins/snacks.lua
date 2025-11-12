d4cb6b3f67d0e34e2a978f5b2e2ce74e291cc6f8
Nischal Shrestha <nischalshresthaofficial@gmail.com>
2025-11-12 14:38:01 +0545

    feat: Enabled displaying hidden and gitignore files in explorer

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true,
+            ignored = true,
+          },
+        },
+        toggles = {
+          hidden = false,
+          ignored = false,
+        },
+      },
+    },
+  },
+}
