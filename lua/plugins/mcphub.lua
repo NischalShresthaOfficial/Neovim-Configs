5f9c07d66178f93ceed13962a813b8363ece7aa1
Nischal Shrestha <nischalshresthaofficial@gmail.com>
2026-04-23 22:19:13 +0545

    Feat: Added MCPHub

---
+++ lua/plugins/mcphub.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "ravitemer/mcphub.nvim",
+    cmd = "MCPHub",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    build = "bundled_build.lua",
+    opts = {
+      use_bundled_binary = true,
+    },
+  },
+}
